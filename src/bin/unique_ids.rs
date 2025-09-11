e1cf6139ffe5e087792cf36a0522708317a5c4be
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2025-09-11 19:55:03 +0200

    feat: Implement unique id generation

---
+++ src/bin/unique_ids.rs
@@ -0,0 +1,45 @@
+use std::collections::HashSet;
+
+use anyhow::Result;
+use mael::Node;
+use serde::{Deserialize, Serialize};
+use ulid::Ulid;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde[tag = "type", rename_all = "snake_case"]]
+enum Request {
+    Init {
+        node_id: String,
+        node_ids: HashSet<String>,
+    },
+    Generate,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum Response {
+    InitOk,
+    GenerateOk { id: Ulid },
+}
+
+struct UniqueIdNode;
+
+impl Node for UniqueIdNode {
+    type Request = Request;
+
+    type Response = Response;
+
+    fn handle(&mut self, request: Self::Request) -> Self::Response {
+        match request {
+            Request::Init { .. } => Response::InitOk,
+            Request::Generate => Response::GenerateOk { id: Ulid::new() },
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let stdin = std::io::stdin().lock();
+    let stdout = std::io::stdout().lock();
+
+    UniqueIdNode.run(stdin, stdout)
+}
